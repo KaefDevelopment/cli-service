4bf97d2f12b91353666f40a50545bc326cd52da2
jaroslav <jaroslav.shashunov@gmail.com>
2023-10-05 19:14:20 +0300

    fix

--- internal/service/events.go
+++ internal/service/events.go
@@ -5,19 +5,11 @@ import (
 )
 
 func (s *CLIService) CreateEvents(events model.Events) error {
-	if err := s.repo.Create(events); err != nil {
-		return err
-	}
-
-	return nil
+	return s.repo.Create(events)
 }
 
 func (s *CLIService) UpdateEvents() error {
-	if err := s.repo.Update(); err != nil {
-		return err
-	}
-
-	return nil
+	return s.repo.Update()
 }
 
 func (s *CLIService) GetKeys() ([]string, error) {
@@ -39,9 +31,5 @@ func (s *CLIService) GetEvents(keys []string) (model.Response, error) {
 }
 
 func (s *CLIService) Delete() error {
-	if err := s.repo.Drop(); err != nil {
-		return err
-	}
-
-	return nil
+	return s.repo.Drop()
 }
